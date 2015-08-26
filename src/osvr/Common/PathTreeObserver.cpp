980e6b78a29147d40eab228212f68a51fbf3b7b0
Ryan Pavlik <ryan@sensics.com>
2015-08-26 18:11:19 -0500

    Add path tree owner and observers

---
+++ src/osvr/Common/PathTreeObserver.cpp
@@ -0,0 +1,50 @@
+/** @file
+    @brief Implementation
+
+    @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include <osvr/Common/PathTreeObserver.h>
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+namespace osvr {
+namespace common {
+
+    void PathTreeObserver::notifyEvent(PathTreeEvents e, PathTreeOwner &tree) {
+        auto &callback = m_handlers[e];
+        if (callback) {
+            callback(tree);
+        }
+    }
+
+    void PathTreeObserver::setEventCallback(PathTreeEvents e,
+                                            callback_type const &callback) {
+        m_handlers[e] = callback;
+    }
+
+} // namespace common
+} // namespace osvr
